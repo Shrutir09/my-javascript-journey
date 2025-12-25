230fde74a6731917c28995e9b8b28db52b7d8f57
Shruti Riya <riyashruti68@gmail.com>
2025-12-25 17:53:33 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,50 +1,32 @@
-let email = document.querySelector("#email");
-let password = document.querySelector("#password");
-let form = document.querySelector("#form");
+let form = document.getElementById("myForm");
+form.addEventListener("submit", function(dets){
+    dets.preventDefault();
 
-let emailError = document.querySelector("#emailError");
-let passwordError = document.querySelector("#passwordError");
-let successMsg = document.querySelector("#successMsg");
+    //read values
+    let name=document.getElementById("name");
+    let email=document.getElementById("email");
+    let message=document.getElementById("message");
+    let country=document.getElementById("country");
 
-const emailRegex = /^[^\s@]+@[^\s@]+\.[a-zA-Z]{2,}$/;
-const passwordRegex =
-  /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    console.log(name,email,message,country);
 
-form.addEventListener("submit", function (e) {
-  e.preventDefault(); 
-
-  emailError.textContent = "";
-  //Purane error / success messages hata deta hai
- //Naya validation clean state se start hota hai
-  passwordError.textContent = "";
-  successMsg.textContent = "";
-
-  email.classList.remove("valid", "invalid");
-  password.classList.remove("valid", "invalid");
-
-  let isValid = true;
-
- 
-  if (!emailRegex.test(email.value)) {
-    emailError.textContent = "Invalid email format";
-    email.classList.add("invalid");
-    isValid = false;
-  } else {
-    email.classList.add("valid");
-  }
-
-  if (!passwordRegex.test(password.value)) {
-    passwordError.textContent = "Password is too weak";
-    password.classList.add("invalid");
-    isValid = false;
-  } else {
-    password.classList.add("valid");
-  }
-
-
-  if (isValid) {
-    successMsg.textContent = "Everything is correct ✅";
-    successMsg.style.color = "green";
-  }
+    if(name.value === ""){
+        alert("Name is required");
+        return;
+    }
+    if(email.value === ""){
+        alert("Email is required");
+        return;
+    }
+    if(message.value === ""){
+        alert("message is required");
+        return;
+    }
+    if(country.value === ""){
+        alert("select your country");
+        return;
+    }
+    else{
+    alert("Form submittted sucessfully");
+    }
 });
-
