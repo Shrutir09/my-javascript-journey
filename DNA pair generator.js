fc6fede845e02b5f5e327664e4a18664898d951f
Shruti Riya <riyashruti68@gmail.com>
2026-06-11 10:21:37 +0530

    add DNA pair generator function to match nucleotide pairs

---
+++ DNA pair generator.js
@@ -0,0 +1,16 @@
+function pairElement(str){
+  let result = [];
+  for(let char of str){
+    if(char === "A"){
+      result.push(["A", "T"]);
+    }else if(char === "T"){
+      result.push(["T", "A"]);
+    }else if(char === "C"){
+      result.push(["C", "G"]);
+    }else if(char === "G"){
+      result.push(["G", "C"]);
+    }
+  }
+  return result;
+}
+console.log(pairElement("ATCG"));
